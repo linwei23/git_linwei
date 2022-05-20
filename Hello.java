7e479c4e57c6b58a8c42347f8c5357eb5d58ea8c
linwei <15327126618@163.com>
2022-05-20 14:40:20 +0800

    master branch modify

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello git init");
-		System.out.println("modify by linwei");
+		System.out.println("modify by linwei!!!");
 		System.out.println("dev branch modify");
+		System.out.println("modify by master branch");
 	}
 }
