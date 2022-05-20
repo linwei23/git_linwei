f7d905a435cdd2f61e521c8ac88dcaab758b4481
linwei <15327126618@163.com>
2022-05-20 14:20:40 +0800

    4th change

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		system.out.println("hello git init");
-		system.out.println("modify by linwei");
+		System.out.println("hello git init");
+		System.out.println("modify by linwei");
 	}
 }
